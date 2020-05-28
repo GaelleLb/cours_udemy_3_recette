aefad0683f8ae0afcf9ec38890dc08d5ab6c22b4
GaelleLb <gaelle.libour@gmail.com>
2020-05-28 12:44:21 +0200

    3rd commit filter

--- src/Controller/AlimentController.php
+++ src/Controller/AlimentController.php
@@ -17,7 +17,9 @@ class AlimentController extends AbstractController
     {
         $aliments = $repo->findAll();
         return $this->render('aliment/aliments.html.twig', [
-            'aliments' => $aliments
+            'aliments' => $aliments,
+            'isCalorie' =>false,
+            'isGlucide' => false
         ]);
     }
 
@@ -28,6 +30,34 @@ class AlimentController extends AbstractController
     {
         return $this->render('aliment/aliment.html.twig', [
             'aliment' => $aliment
+        ]);
+    }
+
+    /**
+     * @Route("/aliments/calories/{calorie}", name="alimentsParCalorie")
+     */
+    public function alimentsParCalorie(AlimentRepository $repo, $calorie)
+    {
+        $aliments = $repo->getAlimentsParPropriete('calorie', '<', $calorie);
+        return $this->render('aliment/aliments.html.twig', [
+            'aliments' => $aliments,
+            'isCalorie' => true,
+            'isGlucide' => false
+
+
+        ]);
+    }
+
+    /**
+     * @Route("/aliments/glucide/{glucide}", name="alimentsParGlucide")
+     */
+    public function alimentsAvecMoinsGlucide(AlimentRepository $repo, $glucide)
+    {
+        $aliments = $repo->getAlimentsParPropriete('glucide', '<', $glucide);
+        return $this->render('aliment/aliments.html.twig', [
+            'aliments' => $aliments,
+            'isCalorie' => false,
+            'isGlucide' => true
         ]);
     }
 }
